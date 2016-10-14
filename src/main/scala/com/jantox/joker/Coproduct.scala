dd4c4e87a9f30835b0b1666a2d968ca619e656f1
Raas Ahsan <agro@jantox.com>
2016-10-14 01:29:34 -0500

    remove stray A.

--- src/main/scala/com/jantox/joker/Coproduct.scala
+++ src/main/scala/com/jantox/joker/Coproduct.scala
@@ -6,7 +6,7 @@ final case class InR[F[_], G[_], A](r: G[A]) extends Coproduct[F, G, A]
 
 object Coproduct {
 
-  implicit def coproductFunctor[F[_]: Functor, G[_]: Functor, A]() = new Functor[({type f[x] = Coproduct[F, G, x]})#f] {
+  implicit def coproductFunctor[F[_]: Functor, G[_]: Functor]() = new Functor[({type f[x] = Coproduct[F, G, x]})#f] {
     def fmap[A, B](f: A => B)(fa: Coproduct[F, G, A]): Coproduct[F, G, B] = fa match {
       case InL(l) => InL(implicitly[Functor[F]].fmap(f)(l))
       case InR(r) => InR(implicitly[Functor[G]].fmap(f)(r))
