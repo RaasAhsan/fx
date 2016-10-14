43b2313b452810e24a2ce797d6ed0b3c25c9d2d0
Raas Ahsan <agro@jantox.com>
2016-10-14 01:27:03 -0500

    implement coproduct functor.

--- src/main/scala/com/jantox/joker/Coproduct.scala
+++ src/main/scala/com/jantox/joker/Coproduct.scala
@@ -3,3 +3,14 @@ package com.jantox.joker
 sealed trait Coproduct[F[_], G[_], A]
 final case class InL[F[_], G[_], A](l: F[A]) extends Coproduct[F, G, A]
 final case class InR[F[_], G[_], A](r: G[A]) extends Coproduct[F, G, A]
+
+object Coproduct {
+
+  implicit def coproductFunctor[F[_]: Functor, G[_]: Functor, A]() = new Functor[({type f[x] = Coproduct[F, G, x]})#f] {
+    def fmap[A, B](f: A => B)(fa: Coproduct[F, G, A]): Coproduct[F, G, B] = fa match {
+      case InL(l) => InL(implicitly[Functor[F]].fmap(f)(l))
+      case InR(r) => InR(implicitly[Functor[G]].fmap(f)(r))
+    }
+  }
+
+}
