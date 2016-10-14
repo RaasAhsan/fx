0a5d5db43254cbb5eb0fa8d4e0e369597b6712fb
Raas Ahsan <agro@jantox.com>
2016-10-14 01:07:32 -0500

    define the free functor.

--- src/main/scala/com/jantox/joker/Free.scala
+++ src/main/scala/com/jantox/joker/Free.scala
@@ -3,3 +3,14 @@ package com.jantox.joker
 sealed trait Free[F[_], A]
 final case class Wrap[F[_], A](f: F[Free[F, A]]) extends Free[F, A]
 final case class Return[F[_], A](a: A) extends Free[F, A]
+
+object Free {
+
+  implicit def freeFunctor[F[_]: Functor]() = new Functor[({type f[x] = Free[F, x]})#f] {
+    def fmap[A, B](f: A => B)(fa: Free[F, A]): Free[F, B] = fa match {
+      case Wrap(wrapped) => Wrap(implicitly[Functor[F]].fmap[Free[F, A], Free[F, B]](freeA => fmap(f)(freeA))(wrapped))
+      case Return(a) => Return(f(a))
+    }
+  }
+
+}
