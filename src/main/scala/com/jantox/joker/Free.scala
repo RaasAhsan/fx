8eacd032ff53e8f523e3abb3f4363c257560a078
Raas Ahsan <agro@jantox.com>
2016-10-16 15:52:33 -0500

    implement free monad.

--- src/main/scala/com/jantox/joker/Free.scala
+++ src/main/scala/com/jantox/joker/Free.scala
@@ -8,8 +8,17 @@ object Free {
 
   implicit def freeFunctor[F[_]: Functor]() = new Functor[({type f[x] = Free[F, x]})#f] {
     def fmap[A, B](f: A => B)(fa: Free[F, A]): Free[F, B] = fa match {
-      case Wrap(wrapped) => Wrap(implicitly[Functor[F]].fmap[Free[F, A], Free[F, B]](freeA => fmap(f)(freeA))(wrapped))
+      case Wrap(wrapped) => Wrap(implicitly[Functor[F]].fmap((freeA: Free[F, A]) => fmap(f)(freeA))(wrapped))
       case Return(a) => Return(f(a))
+    }
+  }
+
+  implicit def freeMonad[F[_]: Functor]() = new Monad[({type f[x] = Free[F, x]})#f] {
+    override def returnM[A](a: A): Free[F, A] = ???
+
+    override def bind[A, B](f: A => Free[F, B])(ma: Free[F, A]): Free[F, B] = ma match {
+      case Wrap(wrapped) => Wrap(implicitly[Functor[F]].fmap((freeA: Free[F, A]) => bind(f)(freeA))(wrapped))
+      case Return(a) => f(a)
     }
   }
 
--- src/main/scala/com/jantox/joker/Monad.scala
+++ src/main/scala/com/jantox/joker/Monad.scala
@@ -1,10 +1,10 @@
 package com.jantox.joker
 
 trait Monad[M[_]] extends Functor[M] {
-  def ret[A](a: A): M[A]
+  def returnM[A](a: A): M[A]
   def bind[A, B](f: A => M[B])(ma: M[A]): M[B]
 
   def fmap[A, B](f: A => B)(fa: M[A]): M[B] = {
-    bind[A, B](a => ret(f(a)))(fa)
+    bind[A, B](a => returnM(f(a)))(fa)
   }
 }
