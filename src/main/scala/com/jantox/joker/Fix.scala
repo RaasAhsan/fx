894dda30c058855c54430443ef6aa2d3df159e3d
Raas Ahsan <agro@jantox.com>
2016-11-06 01:47:50 -0500

    Curry cata and ana.

--- src/main/scala/com/jantox/joker/Fix.scala
+++ src/main/scala/com/jantox/joker/Fix.scala
@@ -6,13 +6,13 @@ object Fix {
 
   // cata :: (f b -> b) -> Fix f -> b
   // cata z (Fix f) = z (fmap (cata z) f)
-  def cata[F[_]: Functor, B](z: F[B] => B): Fix[F] => B = fix => {
+  def cata[F[_]: Functor, B](z: F[B] => B)(fix: Fix[F]): B = {
     z(implicitly[Functor[F]].map(cata(z))(fix.f))
   }
 
   // ana :: (b -> f b) -> b -> Fix f
   // ana z b = Fix (fmap (ana z) (b z))
-  def ana[F[_]: Functor, B](z: B => F[B]): B => Fix[F] = b => {
+  def ana[F[_]: Functor, B](z: B => F[B])(b: B): Fix[F] = {
     Fix[F](implicitly[Functor[F]].map(ana(z))(z(b)))
   }
 
