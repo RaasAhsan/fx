662ce31495d28220af9749fe770b7ef17d9dd4bf
Raas Ahsan <agro@jantox.com>
2017-03-05 19:39:47 -0600

    Implement Bifunctor for Either and Tuple2.

--- src/main/scala/com/jantox/joker/Bifunctor.scala
+++ src/main/scala/com/jantox/joker/Bifunctor.scala
@@ -5,3 +5,24 @@ trait Bifunctor[F[_, _]] {
   def bifmap[A, B, C, D](f: A => B)(g: C => D)(a: F[A, C]): F[B, D]
 
 }
+
+object Bifunctor {
+
+  object EitherBifunctor extends Bifunctor[Either] {
+
+    override def bifmap[A, B, C, D](f: (A) => B)(g: (C) => D)(a: Either[A, C]): Either[B, D] = a match {
+      case Right(r) => Right(g(r))
+      case Left(l) => Left(f(l))
+    }
+
+  }
+
+  object Tuple2Bifunctor extends Bifunctor[Tuple2] {
+
+    override def bifmap[A, B, C, D](f: (A) => B)(g: (C) => D)(a: (A, C)): (B, D) = {
+      (f(a._1), g(a._2))
+    }
+
+  }
+
+}
